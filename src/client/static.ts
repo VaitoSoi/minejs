e9496d47ec6106c052841629b570ff2bb8333648
VaitoSoi <duyduy15456@gmail.com>
2026-06-23 19:42:25 +0700

    feat: add pack and unpack position for better storage ultilization

--- src/client/static.ts
+++ src/client/static.ts
@@ -4,3 +4,27 @@ export const SectionsPerChunk: Record<string, number> = {
     "minecraft:the_end": 16,
     "minecraft:the_nether": 8,
 };
+
+/**
+ * Packed block position within a chunk column to an integer
+ * 
+ * A packed position look like this:
+ * `[9 bits of Y][4 bits of Z][4 bits of X]`
+ * 
+ * @param x relative within chunk column
+ * @param y world height
+ * @param z relative within chunk column
+ */
+export const packBlockPos = (x: number, y: number, z: number) => y << 8 & z << 4 & x;
+
+
+/**
+ * Unpacked block position within a chunk column from an integer
+ * 
+ * @returns unpacked position
+ */
+export const readPackedBlockPos = (packed: number) => ({
+    x: packed & 0xF,
+    z: (packed >> 4) & 0xF,
+    y: (packed >> 8) & 0x1FF,
+});
