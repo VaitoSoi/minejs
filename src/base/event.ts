492c9a0e7d1436f7ffded2101a6aa127b81bf3a0
VaitoSoi <duyduy15456@gmail.com>
2026-07-01 01:50:35 +0700

    fix: wrong args

--- src/base/event.ts
+++ src/base/event.ts
@@ -1,6 +1,6 @@
 // Simpler version of https://github.com/andywer/typed-emitter
 export interface TypedEmmiter<Events extends object> {
-    on: <K extends keyof Events>(event: K, ...args: Events[K] extends any[] ? Events[K] : never) => void,
-    once: <K extends keyof Events>(event: K, ...args: Events[K] extends any[] ? Events[K] : never) => void,
+    on: <K extends keyof Events>(event: K, func: Events[K] extends any[] ? (...args: Events[K]) => void : never) => void,
+    once: <K extends keyof Events>(event: K, func: Events[K] extends any[] ? (...args: Events[K]) => void : never) => void,
     emit: <K extends keyof Events>(event: K, ...args: Events[K] extends any[] ? Events[K] : never) => void,
 }
