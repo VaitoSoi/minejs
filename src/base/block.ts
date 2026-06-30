40dde654bb6e949ebb476abf1f912a7aff907b2b
VaitoSoi <duyduy15456@gmail.com>
2026-07-01 01:47:59 +0700

    feat: add block manager for fetching blocks

---
+++ src/base/block.ts
@@ -0,0 +1,69 @@
+import { TCPClient } from "../client/tcp";
+import { AABB } from "./aabb";
+import { ClientNotReady, RegistryItemNotFound } from "./error";
+import { BlockStateRegistry } from "./registry";
+import { Position, Vec3 } from "./vector";
+
+export class BlockManager {
+    constructor(private client: TCPClient) { }
+
+    /**
+     * Get block at position
+     */
+    public at(x: number, y: number, z: number): string;
+    public at(position: Position): string;
+    public at(a: Position | number, b?: number, c?: number): string {
+        const { x, y, z } = Vec3.loadArgs(a, b, c);
+        const sx = Math.floor(x / 4), sy = Math.floor(y / 4), sz = Math.floor(z / 4);
+        const px = x % 16, py = y % 16, pz = z % 16;
+        const section = this.client.world!.chunks[`${this.client.player!.dimension}${sx}${sz}`]!.sections[sy]?.block;
+        if (!section) return "";
+        if (section.data == null) return section.palette[0]!.toString();
+        const dataArray = section.data!;
+        const bitsPerEntry = section.bpe;
+        const entryIndex = px + (py * 16) + (pz * 16 * 16);
+        const entriesPerLong = Math.floor(64 / bitsPerEntry);
+        const entryMask = (1n << BigInt(bitsPerEntry)) - 1n;
+        const longIndex = entryIndex / entriesPerLong;
+        const bit_index = entryIndex % entriesPerLong * bitsPerEntry;
+        return String((dataArray[longIndex]! >> BigInt(bit_index)) & entryMask);
+    }
+
+    /**
+     * Query the entity in the section that intersec with BB
+     * @param queryBB The querying bounding box
+     * @returns 
+     */
+    public queryAABB(queryBB: AABB) {
+        if (!this.client.isReady())
+            throw new ClientNotReady();
+
+        const { minX, minY, minZ, maxX, maxY, maxZ } = queryBB;
+        const sx0 = Math.floor(minX / 4), sx1 = Math.floor(maxX / 4);
+        const sy0 = Math.floor(minY / 4), sy1 = Math.floor(maxY / 4);
+        const sz0 = Math.floor(minZ / 4), sz1 = Math.floor(maxZ / 4);
+
+        const ids: string[] = [];
+        // Interating through each chunk section
+        for (let sx = sx0; sx <= sx1; sx++)
+            for (let sy = sy0; sy <= sy1; sy++)
+                for (let sz = sz0; sz <= sz1; sz++)
+                    // Interating through each block in each
+                    for (let px = minX - sx * 16; px < Math.min(maxX - sx * 16, (sx + 1) * 16); px++)
+                        for (let py = minY - sy * 16; py < Math.min(maxY - sy * 16, (sy + 1) * 16); py++)
+                            for (let pz = minZ - sz * 16; pz < Math.min(maxZ - sz * 16, (sz + 1) * 16); pz++)
+                                ids.push(this.at(px, py, pz));
+
+        return ids.map((id) => {
+            if (!(id in BlockStateRegistry.mapIdToState))
+                throw new RegistryItemNotFound(`block state ${id}`);
+            const state = BlockStateRegistry.mapIdToState[id]!;
+
+            const bbs = state.boxes.map(val => new AABB(
+                val.minX, val.minY, val.minZ,
+                val.maxX, val.maxY, val.maxZ
+            ));
+            
+        });
+    }
+}
