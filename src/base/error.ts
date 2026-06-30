cfcb9f6343802ab4dcbffffe6c5f4785218b2d41
VaitoSoi <duyduy15456@gmail.com>
2026-07-01 01:49:42 +0700

    feat: auth, client and registy error

--- src/base/error.ts
+++ src/base/error.ts
@@ -24,7 +24,7 @@ export class StringSizeExceedLimit extends Error {
 }
 
 export class InvalidValue extends Error {
-    constructor (public type: string, public got: string) {
+    constructor(public type: string, public got: string) {
         super(`got invalid value for ${type}: ${got}`);
     }
 }
@@ -32,5 +32,71 @@ export class InvalidValue extends Error {
 export class SockerIsNotWritable extends Error {
     constructor() {
         super("socket is not writable, do you connect it before?");
+    }
+}
+
+export class CantGetMsAuthToken extends Error {
+    constructor() {
+        super("can't get microsoft auth token, maybe the responding request is invalid");
+    }
+}
+
+export class CantGetMsAccessToken extends Error {
+    constructor() {
+        super("can't get microsoft access token");
+    }
+}
+
+export class AuthError extends Error {
+    constructor(public error: string) {
+        super(`got oauth error: ${error}, for more info: https://datatracker.ietf.org/doc/html/rfc6749#section-5.2`);
+    }
+}
+
+export class XboxError extends Error {
+    constructor(public code: string) {
+        super(`xbox error code: ${code}`);
+    }
+}
+
+export class ProfileNotFound extends Error {
+    constructor() {
+        super("cant find your minecraft profile, do you own minecraft or set up the profile?");
+    }
+}
+
+export class ProfileError extends Error {
+    constructor(public code: string, public message: string) {
+        super(`profile error: ${code}: ${message}`);
+    }
+}
+
+export class AuthDenied extends Error {
+    constructor() {
+        super("the authorization request was denied");
+    }
+}
+
+export class AuthTokenExpired extends Error {
+    constructor() {
+        super("device token is expired, please try again");
+    }
+}
+
+export class MissingAuthOption extends Error {
+    constructor() {
+        super("you need to provide auth option to be able to play in premium server");
+    }
+}
+
+export class RegistryItemNotFound extends Error {
+    constructor(public item: string) {
+        super(`cant find registry item: ${item}`);
+    }
+}
+
+export class ClientNotReady extends Error {
+    constructor() {
+        super(`client is not ready`);
     }
 }
