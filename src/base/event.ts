cee44dc980b852162c90f3ca2da26376ae4ff691
VaitoSoi <duyduy15456@gmail.com>
2026-07-23 01:02:31 +0700

    chore: change comment to jsdoc for latter docs gen

--- src/base/event.ts
+++ src/base/event.ts
@@ -1,4 +1,4 @@
-// Simpler version of https://github.com/andywer/typed-emitter
+/** @see https://github.com/andywer/typed-emitter */
 export interface TypedEmmiter<Events extends object> {
     on: <K extends keyof Events>(event: K, func: Events[K] extends any[] ? (...args: Events[K]) => void : never) => void,
     once: <K extends keyof Events>(event: K, func: Events[K] extends any[] ? (...args: Events[K]) => void : never) => void,
