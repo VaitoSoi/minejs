055dc0449fff484810ddf679f9dafd6e80b0e186
VaitoSoi <duyduy15456@gmail.com>
2026-07-23 00:41:26 +0700

    chore: just for updating data should automate this :_D

--- test/registry/index.ts
+++ test/registry/index.ts
@@ -1,12 +1,14 @@
-import { readFile, writeFile } from "node:fs/promises";
+import { readFile, writeFile } from "fs/promises";
 
 async function main() {
     const blocks = JSON.parse(await readFile("./assets/minecraft/blocks.json", { encoding: "utf-8" }));
+    const shapes = JSON.parse(await readFile("./assets/minecraft/shape.json", { encoding: "utf-8" }));
 
-    for (const block in blocks) {
-        if (block === "minecraft:honey_block")
-            blocks[block]["definition"]["jump_factor"] = 0.5;
-    }
+    for (const block in blocks)
+        if (block in shapes && "states" in blocks[block])
+            for (const state of blocks[block]["states"])
+                if (state["id"] in shapes[block])
+                    state["boxes"] = shapes[block][state["id"]];
 
     await writeFile("./assets/minecraft/blocks.json", JSON.stringify(blocks, null, 2));
 }
