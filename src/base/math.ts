f7fab38fa20d758ec103be39fcdc29f75711ca2a
VaitoSoi <duyduy15456@gmail.com>
2026-08-11 17:32:49 +0700

    chore: change the return type

--- src/base/math.ts
+++ src/base/math.ts
@@ -11,14 +11,13 @@ export const Epsilon = 1.0e-7; // 1^-7
  * @hidden
  * @see https://github.com/openjdk/jdk/blob/master/src/java.base/share/classes/java/util/UUID.java#L165
  */
-export function computeUUID(playerName: string): Buffer {
+export function computeUUID(playerName: string): string {
     const md5 = createHash("md5").update(playerName).digest();
     md5[6]! &= 0x0f;  /* clear version        */
     md5[6]! |= 0x30;  /* set to version 3     */
     md5[8]! &= 0x3f;  /* clear variant        */
     md5[8]! |= 0x80;  /* set to IETF variant  */
-
-    return md5;
+    return md5.toString("hex");
 }
 
 /**
