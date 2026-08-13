06892e5124fb47147a0b81dfdb91d56d22723242
VaitoSoi <duyduy15456@gmail.com>
2026-08-13 18:07:48 +0700

    chore: update test client

--- test/client/index.ts
+++ test/client/index.ts
@@ -1,22 +1,25 @@
-import { MoveDirection, Client } from "../../src/";
+import { Input, Client } from "../../src/index";
 
 const client = new Client({
     playerName: "bot",
     host: "localhost",
     port: 25565,
     version: "26.2",
-    // debug: {
-    //     packetLogger: true
-    // },
-    isOffline: true,
+    debug: {
+        packetLogger: true
+    },
     loadRegistry: false,
-    // auth: {
-    //     client_id: "d86254d8-edf7-4640-90eb-643c99af188e",
-    //     method: "loopback",
-    //     openBrowser: true
-    // }
+    auth: {
+        client_id: "d86254d8-edf7-4640-90eb-643c99af188e",
+        method: "loopback",
+        openBrowser: true,
+        port: 12345
+    },
+    shouldVerifyMessageOrder: true,
+    shouldVerifyMessageSignature: true
 });
 client.connect();
+client.hold(Input.Jump);
 client.on("disconnect", (...args) => console.dir({ name: "disconnect", args }, { depth: null }));
 client.on("disconnectRaw", (...args) => console.dir({ name: "disconnectRaw", args }, { depth: null }));
 // client.on("loadChunk", (...args) => console.dir({ name: "loadChunk", args }, { depth: null }));
@@ -34,18 +37,22 @@ client.on("message", (message) => {
     if (!message.content.startsWith("_")) return;
     const args = message.content.slice(1).split(" ");
     switch (args[0]) {
-        case "w": client.hold(MoveDirection.Forward); break;
-        case "sw": client.release(MoveDirection.Forward); break;
+        case "w": client.hold(Input.Forward); break;
+        case "sw": client.release(Input.Forward); break;
         case "ws":
-            client.hold(MoveDirection.Forward);
+            client.hold(Input.Forward);
             setTimeout(() => client.disconnect(), 2000);
             break;
-        case "s": client.hold(MoveDirection.Backward); break;
-        case "ss": client.release(MoveDirection.Backward); break;
-        case "a": client.hold(MoveDirection.Left); break;
-        case "sa": client.release(MoveDirection.Left); break;
-        case "d": client.hold(MoveDirection.Right); break;
-        case "sd": client.release(MoveDirection.Right); break;
+        case "s": client.hold(Input.Backward); break;
+        case "ss": client.release(Input.Backward); break;
+        case "a": client.hold(Input.Left); break;
+        case "sa": client.release(Input.Left); break;
+        case "d": client.hold(Input.Right); break;
+        case "sd": client.release(Input.Right); break;
+        case "j": client.hold(Input.Jump); break;
+        case "sj": client.release(Input.Jump); break;
+        case "run": client.hold(Input.Sprint); break;
+        case "srun": client.release(Input.Sprint); break;
         case "stop": client.stopMoving(); break;
 
         case "at": {
@@ -56,7 +63,7 @@ client.on("message", (message) => {
         }
 
         case "disconnect":
-        case "exit": 
+        case "exit":
             client.disconnect();
             break;
     }
